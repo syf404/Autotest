4e7c4e4cf0902396b207bb8fa9db54d94c4ca886
SUN <SYF@qq.cn>
2023-11-14 09:14:07 +0800

    TestNG测试02

--- AUtoTest-02/src/main/java/ExtentreportsTest.java
+++ AUtoTest-02/src/main/java/ExtentreportsTest.java
@@ -1,4 +1,5 @@
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class ExtentreportsTest {
@@ -16,6 +17,12 @@ public class ExtentreportsTest {
     }
     @Test
     public void test04(){
-        Assert.assertEquals(1,1);
+        Assert.assertEquals(1,3);
+    }
+//    抛出一个日志
+    @Test
+    public void runtimelog(){
+        Reporter.log("日志记录");
+        throw new RuntimeException("这是个人抛出的");
     }
 }
